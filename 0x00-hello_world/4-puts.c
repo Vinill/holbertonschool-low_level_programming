45b0a062fb124f6935d68f7f879b5d5f30c97981
Vinill <danimuller758@gmail.com>
2022-02-03 10:38:17 -0800

    5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- *main - entry
- *Return: 0
- */
+ *  *main - entry
+ *   *Return: 0
+ *    */
 int main(void)
 {
-char text[] = "\"Programming is like building a multilingual puzzle";
-puts(text);
-return (0);
+	char text[] = "\"Programming is like building a multilingual puzzle";
+	puts(text);
+	return (0);
 }
+~      
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+ * main - Write a C program that prints exactly..
+ * Return: always 0
+ * */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0)
