4edc8808e9d050af64419ac42bf59e85bf31a0d0
Vinill <danimuller758@gmail.com>
2022-02-04 08:57:25 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Muestra el abecedario
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z' ; ch++ "\n")
+	{
+		putchar(ch);
+	}
+	return (0);
+}
