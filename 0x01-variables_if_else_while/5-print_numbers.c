f2cef82fe9edb2b3bbc17eec0cd8d2e92e720971
Vinill <danimuller758@gmail.com>
2022-02-04 10:30:27 -0800

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Muestra el abecedario exepto q y e
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int in;
+
+	for (in = 0 ; in <= 9 ; in++)
+	{
+		putchar(in);
+	}
+	putchar('\n');
+	return (0);
+}
