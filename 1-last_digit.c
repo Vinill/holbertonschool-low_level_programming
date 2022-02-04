12c8b26d555859cac240e815b50c729ef8498829
Vinill <danimuller758@gmail.com>
2022-02-04 07:27:14 -0800

    0 and 1

---
+++ 1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - Encuantra el ultimo digito
+ * Return: Devuelve 0
+ */
+int main(void)
+{
+	int n, lastDigit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	lastDigit = n % 10;
+
+	if (lastDigit > 5)
+	{
+		printf("last digit of %d is %d greater than 5", n, lastDigit);
+	}
+	else if (lastDigit == 0)
+	{
+		printf("last digit of %d is %d greater than 5", n, lastDigit);
+	}
+	if (lastDigit < 6)
+	{
+		printf("last digit of %d is %d greater than 5", n, lastDigit);
+	}
+	return (0);
+}
