a96a39fdaf7e9dbb9760069de3a6d3e56b51e379
Vinill <danimuller758@gmail.com>
2022-02-04 11:56:21 -0800

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,13 +9,13 @@ int main(void)
 	char ch;
 	int in;
 
+	for (in = 1; in <= 9 ; in++)
+	{
+		putchar(in + '0');
+	}
 	for (ch = 'a'; ch <= 'f' ; ch++)
 	{
 		putchar(ch);
-	}
-	for (in = 0; in <= 9 ; in++)
-	{
-		putchar(in);
 	}
 	putchar('\n');
 	return (0);
