72929974e7de6cc3d4a40b6b1f6345b4a6746f13
Vinill <danimuller758@gmail.com>
2022-02-07 07:35:41 -0800

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,17 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * main - check the code
+ * print_alphabet - Imprimir abecedario
  *
- * Return: Always 0.
  */
 void print_alphabet(void)
 {
-	char ch;
+	int i;
+	char ch[] = "abcdefghijklmnopqrstuvwxyz";
 
+	for (i = 0; i <= 25; i++)
+	{
+		_putchar(ch[i]);
+	}
+	_putchar ('\n');
 }
