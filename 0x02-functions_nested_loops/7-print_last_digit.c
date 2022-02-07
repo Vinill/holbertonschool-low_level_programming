29a476ddeb6ef8158cb12202c34e1f0cdde637ce
Vinill <danimuller758@gmail.com>
2022-02-07 11:41:04 -0800

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_last_digit - Devuelve el ultimo digito de n
+ *
+ * @n: Is a var
+ *
+ * Return: Num
+ */
+
+int print_last_digit(int n)
+{
+	int lastDigit;
+
+	lastDigit = n % 10
+	_putchar(lastDigit + '0');
+	
+	return(lastDigit);
+}
