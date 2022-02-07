c3ce3b99a883f2695a02910d2d3e827989dc70f1
Vinill <danimuller758@gmail.com>
2022-02-07 10:35:09 -0800

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _sign - Devuelve 0 o 1 si es minuscula
+ *
+ * @n: Is var
+ *
+ * Return: si es minuscula retorna 0 sino 1
+ */
+
+int _sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ("+");
+		return (1);
+	}
+	if else (n == 0)
+	{
+		_putchar ("0");
+		return (0);
+	}
+	else (n < 0)
+	{
+		_putchar ("-");
+		return (-1);
+	}
+}
