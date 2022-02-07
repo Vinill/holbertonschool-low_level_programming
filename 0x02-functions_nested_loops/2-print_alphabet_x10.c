dd92ac3dad9191b328133513c1f6ad6211ad8d05
Vinill <danimuller758@gmail.com>
2022-02-07 08:59:48 -0800

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - Imprimir abecedario 10 veces
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char ch[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (i = 0; i <= 25; i++)
+		{
+			_putchar(ch[i]);
+		}
+	}
+	_putchar ('\n');
+}
