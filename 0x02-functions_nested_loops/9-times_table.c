b6c45a3f4162592e4fe6b6db407374a79a2e2786
Vinill <danimuller758@gmail.com>
2022-02-07 14:44:44 -0800

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ *jack_bauer - Devuelve el ultimo digito de n
+ *
+ *Return: Table 9
+ */
+void times_table(void)
+{
+	int line;
+	int column
+	int num;
+
+	for (line = 0; line <= 9; line++)
+	{
+		for (column = 0; column <= 9; column++)
+			num = (line * column);
+			if (column == 0)
+			{
+				_putchar(' ');
+				_putchar(',');
+				_putchar(num + '0')
+			}
+			else if (column <= 9)
+			{
+				_putchar(' ');
+				_putchar(',');
+				_putchar(num + '0');
+			}
+			else if (num > 9)
+			{
+				_putchar(' ');	
+				_putc
+				_putchar('0' + (num / 10));
+				_putchar('0' + (num % 10));
+			}
+			return ('\n');
+	}
+}
