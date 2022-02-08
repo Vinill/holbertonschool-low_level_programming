c41a6454fd77ce42257023d2b48d2383c5cae2cb
Vinill <danimuller758@gmail.com>
2022-02-08 05:33:45 -0800

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - sumar "x" con "y" e imprime su resultado
+ * @x: numero x
+ * @y: numero y
+ * Return: retornara el resultado de la suma de "x" e "y"
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
