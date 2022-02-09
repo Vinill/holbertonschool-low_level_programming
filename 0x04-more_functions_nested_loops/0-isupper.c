513390e9d8c4a15f3afe1695c07bf6516411eb2e
Vinill <danimuller758@gmail.com>
2022-02-09 07:59:57 -0800

    1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_isupper - Print
+ *@c: Is a variable
+ *Return: Isupper
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
