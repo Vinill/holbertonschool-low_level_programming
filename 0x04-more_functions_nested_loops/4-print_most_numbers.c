9ec1d7ba7af4b73afdfb35ec9e1f43394634fad2
Vinill <danimuller758@gmail.com>
2022-02-09 11:43:21 -0800

    Ejercice 4 correct

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - Prints numbers exepted 2 and 4
+ *
+ *_putchar: Result of print
+ *
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num == 4 || num == 2)
+		{
+			num++;
+		}
+	_putchar(num + '0');
+	_putchar('\n');
+	}
+}
