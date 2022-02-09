d3d5e8fbebff01ec4193e1f4cac88168506676cf
Vinill <danimuller758@gmail.com>
2022-02-09 14:40:52 -0800

    Yeah

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *print_diagonal - Write a diagonal
+ *
+ *@n: Is a variable
+ *
+ *_putchar: Is a lines and spaces
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int num;
+	int space;
+
+	if (n > 0)
+	{
+		for (num = 1; num <= n; num++)
+		{
+			for (space = 1; space < num; space++)
+			{	
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
