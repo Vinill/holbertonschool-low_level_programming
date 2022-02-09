c00473f91245c287b50c8d32f430d895c5971fc5
Vinill <danimuller758@gmail.com>
2022-02-09 08:36:36 -0800

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isdigit - Devuelve el ultimo digito de n
+ *
+ * @c: Is a var
+ *
+ * Return: Num
+ */
+
+int _isdigit(int c)
+{
+	int c;
+
+	if (c < 9)
+	{
+		return(1);
+	}
+	else 
+	{
+		return(0);
+	}
+}
