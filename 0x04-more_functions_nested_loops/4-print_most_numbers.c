9e745d43ad18a01d3026cfb311aa653d14a5e97c
Vinill <danimuller758@gmail.com>
2022-02-09 17:38:07 -0800

    fixed 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -15,10 +15,9 @@ void print_most_numbers(void)
 	for (num = 0; num <= 9; num++)
 	{
 		if (num == 4 || num == 2)
-		{
 			num++;
-		}
 	_putchar(num + '0');
-	_putchar('\n');
 	}
+	_putchar('\n');
+	
 }
