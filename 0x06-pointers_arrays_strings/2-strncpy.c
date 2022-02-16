10c5cf25b6eb092e26b2efbf41b57f706e414cd6
Vinill <danimuller758@gmail.com>
2022-02-16 12:29:36 +0000

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- *_strncpy - Code for return the value n
- *@dest: Value
- *@src: Value two
- *@n: Valor of n
- *Return: Return dest
+ * _strncpy - Code for return the value n
+ * @dest: Value
+ * @src: Value two
+ * @n: Valor of n
+ * Return: Return dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
+
 	int index;
 
 	for (index = 0; index < n && src[index] != '\0'; index++)
-	dest[index] = src[index];
-
+		dest[index] = src[index];
 	while (index < n)
 	{
 		dest[index] = '\0';
