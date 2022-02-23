574ec4eccb6a8aef58f264a5c3686d491497d0a7
Vinill <danimuller758@gmail.com>
2022-02-23 14:15:52 +0000

    Ejercice 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * @s: Is a pointer
+ *
+ * _puts_recursion - Funcion that print a string
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+
+	_putchar('\n');
+}
