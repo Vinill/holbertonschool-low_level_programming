277ed5627c9421d9c1e8a859a6df1d805e67d50e
Vinill <danimuller758@gmail.com>
2022-03-03 14:44:17 +0000

    Ejercice 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *malloc_checked - Function that allocates memory using malloc
+ *
+ *@b: Var of type unsigned int
+ *
+ *Return: mem
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *mem = malloc(b);
+
+	if(mem == NULL)
+		exit(98);
+
+	return(mem);
+}
