324b85ce95066b1137e1139581ad76e3e75d0b8f
Vinill <danimuller758@gmail.com>
2022-03-08 18:47:34 +0000

    fixed 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,18 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO_H
+#ifndef ABS_H
+#define ABS_H
 
-#define FUNCTION_LIKE_MACRO_H
-
-/**
- * File: 3-function_like_macro.h
- *
- * Auth: Vinill
- *
- * @x: Is a num
- *
- * Desc: function-like macro ABS(x)
- *	that computes the absolute value of a number x
- */
-
-#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define ABS(x) (((x) < 0) ? -(x) : (x))
 
 #endif
