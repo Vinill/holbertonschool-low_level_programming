8833f5dfdb168d36e4ab8a1210092079fb05a7b0
Vinill <danimuller758@gmail.com>
2022-03-08 18:27:41 +0000

    fixed 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-iifndef FUNCTION_LIKE_MACRO_H
+#ifndef FUNCTION_LIKE_MACRO_H
 
 #define FUNCTION_LIKE_MACRO_H
 
@@ -13,6 +13,6 @@ iifndef FUNCTION_LIKE_MACRO_H
  *	that computes the absolute value of a number x
  */
 
-#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define ABS(x) ((x) < 0 ? - (x) : (x))
 
 #endif
