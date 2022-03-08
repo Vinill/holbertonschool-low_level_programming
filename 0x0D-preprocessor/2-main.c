1ceec364c6d5c430885265bc4bec4e3cc173d292
Vinill <danimuller758@gmail.com>
2022-03-08 17:03:53 +0000

    fixed Ejercice 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -5,7 +5,7 @@
  *
  * Auth: Vinill
  *
- * main - prints the name of the file it 
+ * main - prints the name of the file it
  *	was compiled from, followed by a new line
  *
  * Return: 0
@@ -14,5 +14,5 @@
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	return 0;
+	return (0);
 }
