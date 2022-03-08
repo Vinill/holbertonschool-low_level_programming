e0a6b2a0201109a445709e4ed2c99973328cdff8
Vinill <danimuller758@gmail.com>
2022-03-08 18:59:08 +0000

    Ejercice 4

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,18 +1,7 @@
-#ifndef FUNCTION_LIKE_MACRO_H
+#ifndef SUM_H
 
-#define FUNCTION_LIKE_MACRO_H
+#define SUM_H
 
-/**
- * File: 3-function_like_macro.h
- *
- * Auth: Vinill
- *
- * @x: Is a x
- *
- * Desc: function-like macro ABS(x)
- *      that computes the absolute value of a number x
- */
-
-#define ABS(x) (((x) < 0) ? -(x) : (x))
+#define SUM(x, y) ((x) + (y))
 
 #endif
