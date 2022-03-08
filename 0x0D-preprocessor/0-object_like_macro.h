7335402ae5ed2ee6ead85d0e8b02ee4fb6ffb3c6
Vinill <danimuller758@gmail.com>
2022-03-08 16:28:21 +0000

    Fixed betty

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -8,7 +8,7 @@
  * Auth: Vinill
  *
  * Desc: header file that defines a macro named SIZE
- * 	 as an abbreviation for the token 1024
+ *	as an abbreviation for the token 1024
  */
 
 #define SIZE 1024
