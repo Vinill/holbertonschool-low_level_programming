5cec7a90e718cd7710f4f2757d75783c02b47b51
Vinill <danimuller758@gmail.com>
2022-03-10 15:01:51 +0000

    Ejercice 0 and 1 good

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ *sum_them_all - sum of all its parameters
+ *
+ *@n: number of arguments
+ *
+ *@...; n parameters
+ *
+ *Return: The sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int index, sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(nums, n);
+	
+	for (index = 0; index < n; index++)
+	{
+		sum += va_arg(nums, int);
+	}
+
+	va_end(nums);
+
+	return (sum);
+}
