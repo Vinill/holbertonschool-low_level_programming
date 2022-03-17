8e4bfac614f09f986c2fefc7474c3abc2761265a
Vinill <danimuller758@gmail.com>
2022-03-17 12:41:20 -0700

    Fixed ejercice 2 and correct ouput

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ *add_node - unction that adds a new node at the beginning of a list_t list
+ *
+ *@head: Pointer to pointer to linked list
+ *
+ *@str: pointer to string in previous first node
+ *
+ *Return: address of new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int i = 0;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	while (str[i])
+		i++;
+
+	new->str = strdup(str);
+	new->len = i;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
+/**
+unsigned int _str_len(const char *string)
+{
+	unsigned int i = 0;
+
+	while (string[i] != '\0')
+		i++;
+
+	return (i);
+}
+*/
