e2f1cd862b16c73da6b334546c57f39bf048065b
Vinill <danimuller758@gmail.com>
2022-03-28 20:54:19 +0000

    Fixed ejercice one

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * create_file - Function that reads a text file
+ * 	and prints it to the POSIX standard output
+ *
+ *@filname: Const var of type char
+ *
+ *@text_content: Content of the text
+ *
+ *Return: 
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t wt, op;
+	unsigned int len = 0;
+
+	if (filename == NULL)
+		return (0);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	op = open(filename, O_TRUNC, O_CREAT | O_RDWR, 0600);
+
+	len = strlen(text_content);
+
+	wt = write(op, text_content, len);
+
+	if (op == -1 || wt == -1)
+		return (-1);
+
+	close(op);
+	return (-1);
+}
