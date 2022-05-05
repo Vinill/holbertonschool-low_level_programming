5f5e02f571ff278449dfa81b47e763e29154319d
Vinill <danimuller758@gmail.com>
2022-05-05 22:15:38 +0000

    Ejercice 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,19 +1,20 @@
 #include "lists.h"
 
 /**
- * print_dlistint - returns the number of elements in a linked dlistint_t list.
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
  * @h: Node head
  *
  * Return: Number of nodes in the list.
  */
+
 size_t dlistint_len(const dlistint_t *h)
 {
-    unsigned int nodes = 0;
+	unsigned int nodes = 0;
 
 	while (h)
 	{
-	h = h->next;
-        nodes++;
+		h = h->next;
+		nodes++;
 	}
 
 	return (nodes);
