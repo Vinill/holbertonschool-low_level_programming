69685f5391e62e5b2886a3d4b995337c1f8138f1
Vinill <danimuller758@gmail.com>
2022-05-09 12:04:22 -0300

    fin

--- 0x18-dynamic_libraries/functions3.c
+++ 0x18-dynamic_libraries/functions3.c
@@ -18,24 +18,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 }
 
 /**
- *_strchr - Is a function that print last of l
- *@c: Its pointer of type char
- *@s: Its pointer of type char
- *Return: Return s + index
- */
-char *_strchr(char *s, char c)
-{
-	int index;
-
-	for (index = 0; s[index] >= '\0'; index++)
-	{
-		if (s[index] == c)
-			return (s + index);
-	}
-	return ('\0');
-}
-
-/**
  *_strspn - Is a function that print last of l
  *@s: Its pointer of type char
  *@accept: Its pointer of type char
