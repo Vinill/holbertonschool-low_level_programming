270896a767a6583e1cdea5aef6c4554a4cc59692
Vinill <danimuller758@gmail.com>
2022-05-27 10:33:51 -0300

    fixed pep8

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,12 +1,12 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Function that creates a hash table
- *
- * @size: Size of the array
- * 
- * Return: A pointer of hash table
- */
+* hash_table_create - Function that creates a hash table
+*
+* @size: Size of the array
+* 
+* Return: A pointer of hash table
+*/
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
